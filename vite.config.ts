8bc2aa0a755705d875a82e8a064d5a26f4296444
shibuya-r <shibuyaryusuke@gmail.com>
2024-06-22 20:44:25 +0900

    add build and deploy wf

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
+  base: "/web-push-notification/",
   plugins: [
     remix({
       future: {
@@ -10,6 +13,16 @@ export default defineConfig({
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
       },
+      ssr: false,
+      basename: "/repository-name/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+      copyFileSync(
+        join(buildPath, "index.html"),
+        join(buildPath, "404.html"),
+      );
+   }
     }),
     tsconfigPaths(),
   ],
