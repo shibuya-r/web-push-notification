b4abe562fe38aa56c6fde6e2f785a638ad1aeeaf
shibuya-r <shibuyaryusuke@gmail.com>
2024-06-22 23:03:32 +0900

    add service worker

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -8,6 +8,13 @@ import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    // we will register it after the page complete the load
+    void navigator.serviceWorker.register("/sw.js");
+  });
+}
+
 startTransition(() => {
   hydrateRoot(
     document,
@@ -16,9 +23,3 @@ startTransition(() => {
     </StrictMode>
   );
 });
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
